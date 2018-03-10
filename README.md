29ae7b3a4fe1cbaebbae59cf98edd2f69265dab5
Dulio Denis <dulio.denis@yahoo.com>
2018-03-09 23:33:47 -0500

    Added icons and titles to the TabBarController

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Thundera
 Thundera is an iOS 11 / Swift 4 based podcast audio player app that lets you search for and bookmark available Podcasts on iTunes, and has full support for offline audio playback.
 
+![](art/screenshot/thundera-00.png?raw=true)
+
 ### Contributing
 See [CONTRIBUTING](CONTRIBUTING.md).
 
--- Thundera/Thundera/Controllers/MainTabBarController.swift
+++ Thundera/Thundera/Controllers/MainTabBarController.swift
@@ -12,7 +12,40 @@ class MainTabBarController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(red: 85.0/255, green: 37.0/255, blue: 131.0/255, alpha: 1.0)
+        UINavigationBar.appearance().prefersLargeTitles = true
+        
+        let thunderPurple = UIColor(red: 85.0/255, green: 37.0/255, blue: 131.0/255, alpha: 1.0)
+        
+        view.backgroundColor = thunderPurple
+        tabBar.tintColor = thunderPurple
+        
+        setupViewControllers()
+    }
+    
+    
+    // MARK: - Setup Method
+    
+    fileprivate func setupViewControllers() {
+        viewControllers = [
+            generateNavigationController(for: ViewController(), title: "Favorites", image: #imageLiteral(resourceName: "favorites")),
+            generateNavigationController(for: ViewController(), title: "Search", image: #imageLiteral(resourceName: "search")),
+            generateNavigationController(for: ViewController(), title: "Downloads", image: #imageLiteral(resourceName: "downloads"))
+        ]
+    }
+    
+    
+    // MARK: - Helper Method
+    
+    fileprivate func generateNavigationController(for rootViewController: UIViewController,
+                                                  title: String,
+                                                  image: UIImage) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        
+        rootViewController.title = title
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        
+        return navController
     }
     
 }
