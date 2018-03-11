d0767e0470810d7dd09f37cfa8ba3c1f4a5e6cbe
Dulio Denis <dulio.denis@yahoo.com>
2018-03-10 20:20:04 -0500

    Use the Decodable Protocol to parse the JSON returned from the iTunes API

--- Thundera/Thundera/Controllers/SearchController.swift
+++ Thundera/Thundera/Controllers/SearchController.swift
@@ -7,13 +7,14 @@
 //
 
 import UIKit
+import Alamofire
 
 class SearchController: UITableViewController {
     
-    let podcasts = [
-        Podcast(name: "Syntax - Tasty Web Development Treats", artistName: "Wes Bos & Scott Tolinski"),
-        Podcast(name: "The /Filmcast", artistName: "David Chen, Devindra Hardawar, and Jeff Cannata"),
-        Podcast(name: "Podcast14", artistName: "Podcast14 Team")
+    var podcasts = [
+        Podcast(trackName: "Syntax - Tasty Web Development Treats", artistName: "Wes Bos & Scott Tolinski"),
+        Podcast(trackName: "The /Filmcast", artistName: "David Chen, Devindra Hardawar, and Jeff Cannata"),
+        Podcast(trackName: "Podcast14", artistName: "Podcast14 Team")
     ]
     
     let cellID = "cellID"
@@ -50,7 +51,8 @@ class SearchController: UITableViewController {
         let podcast = podcasts[indexPath.row]
         
         cell.imageView?.image = #imageLiteral(resourceName: "podcast-default-cover-art")
-        cell.textLabel?.text = "\(podcast.name)\nby \(podcast.artistName)"
+        // Use the Nil-Coalescing Operator
+        cell.textLabel?.text = "\(podcast.trackName ?? "")\nby \(podcast.artistName ?? "")"
         cell.textLabel?.numberOfLines = -1
         
         return cell
@@ -61,7 +63,37 @@ class SearchController: UITableViewController {
 extension SearchController: UISearchBarDelegate {
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        print(searchText)
+        let url = "https://itunes.apple.com/search?term=\(searchText)"
+        Alamofire.request(url).responseData { (dataResponse) in
+            if let err = dataResponse.error {
+                print("Failed to contact server: \(err.localizedDescription)")
+                return
+            }
+            
+            guard let data = dataResponse.data else { return }
+             //use to look at JSON result
+             //let responseString = String(data: data, encoding: .utf8)
+             //print(responseString ?? "")
+            
+            do {
+                let searchResult = try JSONDecoder().decode(SearchResults.self, from: data)
+                print("Search Result: \(searchResult.resultCount)")
+                searchResult.results.forEach({ (podcast) in
+                    print("Name: \(podcast.trackName ?? "")")
+                })
+                
+                self.podcasts = searchResult.results
+                self.tableView.reloadData()
+                
+            } catch let decodeError {
+                print("Failed to decode: \(decodeError.localizedDescription)")
+            }
+        }
+    }
+    
+    struct SearchResults: Decodable {
+        let resultCount: Int
+        let results: [Podcast]
     }
     
 }
--- Thundera/Thundera/Models/Podcast.swift
+++ Thundera/Thundera/Models/Podcast.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct Podcast {
-    let name: String
-    let artistName: String
+struct Podcast: Decodable {
+    var trackName: String?
+    var artistName: String?
 }
