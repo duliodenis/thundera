e0a1c3ae362209d2c538c812b70fc0612692ffaa
Dulio Denis <dulio.denis@yahoo.com>
2018-03-13 17:31:08 -0400

    Parse XML feed with FeedKit to display episodes in tableView

--- Thundera/Thundera/Controllers/EpisodesController.swift
+++ Thundera/Thundera/Controllers/EpisodesController.swift
@@ -7,18 +7,75 @@
 //
 
 import UIKit
+import FeedKit
 
 class EpisodesController: UITableViewController {
+    
+    var episodes = [Episode]()
     
     var podcast: Podcast? {
         didSet {
             navigationItem.title = podcast?.trackName
+            fetchEpisodes()
         }
+    }
+    
+    struct Episode {
+        let title: String?
+    }
+    
+    fileprivate func fetchEpisodes() {
+        guard let feedURL = podcast?.feedUrl else { return }
+        
+        // to prevent ATP Errors - if http - switch to https
+        let secureFeedURL = feedURL.contains("https") ? feedURL : feedURL.replacingOccurrences(of: "http", with: "https")
+        
+        guard let url = URL(string: secureFeedURL) else { return }
+        let parser = FeedParser(URL: url)
+        
+        parser?.parseAsync(result: { (result) in
+            switch result {
+            case let .rss(feed):
+                var episodes = [Episode]()
+                feed.items?.forEach({ (feedItem) in
+                    episodes.append(Episode(title: feedItem.title ?? ""))
+                })
+                self.episodes = episodes
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+                break
+                
+            default:
+                print("something else")
+            }
+        })
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableView()
+    }
+    
+    
+    // MARK: - Helper Method
+    fileprivate func setupTableView() {
         tableView.separatorStyle = .none
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+    }
+    
+    
+    // MARK: TableView Delegate Methods
+    fileprivate let cellID = "cellID"
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return episodes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        cell.textLabel?.text = episodes[indexPath.row].title
+        return cell
     }
     
 }
