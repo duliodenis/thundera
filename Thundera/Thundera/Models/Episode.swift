92b3aa85cc979f2b438b606d128f4c04c58c3884
Dulio Denis <dulio.denis@yahoo.com>
2018-03-17 20:34:10 -0400

    Added playback controls to the PlayerView UI

--- Thundera/Thundera/Models/Episode.swift
+++ Thundera/Thundera/Models/Episode.swift
@@ -11,12 +11,14 @@ import FeedKit
 
 struct Episode {
     let title: String
+    let author: String
     let pubDate: Date
     let description: String
     var imageUrl: String?
     
     init(feedItem: RSSFeedItem) {
         self.title = feedItem.title ?? ""
+        self.author = feedItem.iTunes?.iTunesAuthor ?? ""
         self.pubDate = feedItem.pubDate ?? Date()
         self.description = feedItem.iTunes?.iTunesSubtitle ?? feedItem.description ?? ""
         self.imageUrl = feedItem.iTunes?.iTunesImage?.attributes?.href
--- Thundera/Thundera/Views/PlayerView.swift
+++ Thundera/Thundera/Views/PlayerView.swift
@@ -11,15 +11,30 @@ import UIKit
 class PlayerView: UIView {
     
     @IBOutlet weak var episodeImageView: UIImageView!
-    @IBOutlet weak var episodeTitleLabel: UILabel!
+    
+    @IBOutlet weak var episodeTitleLabel: UILabel! {
+        didSet {
+            episodeTitleLabel.numberOfLines = 2
+        }
+    }
+    
+    @IBOutlet weak var authorLabel: UILabel!
     
     var episode: Episode! {
         didSet {
             episodeTitleLabel.text = episode.title
+            authorLabel.text = episode.author
             
             guard let url = URL(string: episode.imageUrl ?? "") else { return }
             episodeImageView.sd_setImage(with: url)
         }
+    }
+    
+    
+    // MARK: - Player Action Methods
+    
+    @IBAction func playPause(_ sender: Any) {
+        
     }
     
     @IBAction func dismissPlayer(_ sender: Any) {
